eebb0cc6d0ae18a70e6c2128b2e665437bb9bbf2
Alexander Aguilar <alexander.aguilar.torrico@gmail.com>
2024-01-11 09:53:43 -0400

    Deploy Drupal Docker

---
+++ deploy.redhat.local.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+  
+echo "###Deploy Redhat###"
+echo "Init creating ssh key to verify conectivity...."
+
+sudo dnf update -y
+
+sudo dnf install -y ansible git
+
+ssh-keygen -q -b 2048 -t rsa -N "" -C "creating SSH" -f KEY-UBUNTU
+
+chmod 400 KEY-REDHAT
+
+cat KEY-REDHAT.pub >> ~/.ssh/authorized_keys
+
+git clone https://github.com/alexolomeo/drupal.docker.git
+
+ansible -i hosts -m ping all
+
+ansible-playbook -i hosts deploy.yml -v
